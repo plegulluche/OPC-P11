f1a1cf90e47d41f2d53af9dcdb59803e6fc7ad3e
le gulluche <plegulluche@gmail.com>
2021-11-29 02:44:36 +0100

    modif de static_cdn en staticfiles

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -141,8 +141,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
